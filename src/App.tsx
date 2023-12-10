2e3cdfa11bf2cb4d56526efecc94372aafee9049
Rostyslav Liashenko <rostyslav.liashenko@unibrix.com>
2023-12-10 17:01:52 +0200

    feat: add tab navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 import ChatScreen from "@app/screens/ChatScreen/ChatScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import AboutUsScreen from "@app/screens/AboutUsScreen/AboutUsScreen";
-import ProfileScreen from "@app/screens/ProfileScreen/ProfileScreen";
+import TabNavigation from "@app/navigation/TabNavigation/TabNavigation";
 
 const Drawer = createDrawerNavigator();
 
@@ -12,10 +11,21 @@ function App(): React.ReactElement {
   return (
     <>
       <NavigationContainer>
-        <Drawer.Navigator initialRouteName='AboutUs'>
-          <Drawer.Screen name='AboutUs' component={AboutUsScreen} />
-          <Drawer.Screen name='Profile' component={ProfileScreen} />
-          <Drawer.Screen name='Chat' component={ChatScreen} />
+        <Drawer.Navigator initialRouteName='Info'>
+          <Drawer.Screen
+            name='Info'
+            component={TabNavigation}
+            options={{
+              drawerLabel: 'Інфо',
+            }}
+          />
+          <Drawer.Screen
+            name='Chat'
+            component={ChatScreen}
+            options={{
+              drawerLabel: 'Чат',
+            }}
+          />
         </Drawer.Navigator>
       </NavigationContainer>
     </>
