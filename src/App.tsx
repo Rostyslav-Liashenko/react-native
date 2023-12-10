0d1d0e94280ebdabacc3ebf274f977f8c2a480f9
Rostyslav Liashenko <rostyslav.liashenko@unibrix.com>
2023-12-10 17:41:26 +0200

    feat: add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import ChatScreen from "@app/screens/ChatScreen/ChatScreen";
-import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import TabNavigation from "@app/navigation/TabNavigation/TabNavigation";
-import CustomDrawerContent from "@app/navigation/CustomDrawerContent/CustomDrawerContent";
-
-const Drawer = createDrawerNavigator();
+import Navigation from "@app/navigation/Navigation/Navigation";
 
 function App(): React.ReactElement {
   return (
     <>
-      <NavigationContainer>
-        <Drawer.Navigator
-          initialRouteName='Info'
-          drawerContent={(props) => <CustomDrawerContent {...props} />}
-        >
-          <Drawer.Screen
-            name='Info'
-            component={TabNavigation}
-            options={{
-              drawerLabel: 'Інфо',
-            }}
-          />
-          <Drawer.Screen
-            name='Chat'
-            component={ChatScreen}
-            options={{
-              drawerLabel: 'Чат',
-            }}
-          />
-        </Drawer.Navigator>
-      </NavigationContainer>
+      <Navigation />
     </>
   );
 }
